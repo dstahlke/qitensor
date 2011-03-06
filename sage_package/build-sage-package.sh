ac21a5fc7d4c967dda858778655f75c40a7db99a
Dan Stahlke <dstahlke@gmail.com>
2011-03-06 13:38:59 -0500

    spkg builder

---
+++ sage_package/build-sage-package.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+fatal() {
+	echo Fatal error.
+	exit 1
+}
+
+ver=$(PYTHONPATH="..:$PYTHONPATH" python -c 'import qitensor; print qitensor.__version__') || fatal
+build="qitensor-$ver"
+mkdir -p $build || fatal
+cp SPKG.txt spkg-install $build || fatal
+rsync -av --delete .. $build/src --exclude $build --exclude .git --exclude "*.spkg" || fatal
+sage --spkg $build || fatal
+ls -la $build.spkg || fatal
+echo Done.

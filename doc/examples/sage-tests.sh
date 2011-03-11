a9af293738cd7e77879a1716a83985c2baeb7e4d
Dan Stahlke <dstahlke@gmail.com>
2011-03-11 16:53:03 -0500

    better sage test script

--- doc/examples/sage-tests.sh
+++ doc/examples/sage-tests.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
-(echo :: ; grep '^ ' group_indices.rst |perl -p -e 's/ # docte.*//; s/>>>/sage:/') > tmp1.rst
-sage -t tmp1.rst
-(echo :: ; grep '^ ' symbolic.rst |perl -p -e 's/ # docte.*//; s/>>>/sage:/') > tmp2.rst
-sage -t tmp2.rst
-rm tmp[12].rst
+fatal() {
+	echo Fatal error.
+	exit 1
+}
+
+run_test() {
+	rst_fn=$1
+	echo "Testing $rst_fn"
+
+	sage_fn=sage-$rst_fn
+	test -e $rst_fn || fatal
+
+	(echo :: ; grep '^ ' $rst_fn |perl -p -e 's/ # docte.*//; s/>>>/sage:/') > $sage_fn
+	test -e $sage_fn || fatal
+
+	sage -t $sage_fn
+	rm -f $sage_fn
+}
+
+run_test group_indices.rst
+run_test symbolic.rst

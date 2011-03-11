c46a919ac1a9bbcc24a5e054ae7ff85e46401e28
Dan Stahlke <dstahlke@gmail.com>
2011-03-11 17:50:45 -0500

    misc

--- doc/examples/run-all-sage-tests.sh
+++ doc/examples/run-all-sage-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-run-sage-test.sh group_indices.rst
-run-sage-test.sh symbolic.rst
-run-sage-test.sh graph_codes.rst
+./run-sage-test.sh group_indices.rst
+./run-sage-test.sh symbolic.rst
+./run-sage-test.sh graph_codes.rst

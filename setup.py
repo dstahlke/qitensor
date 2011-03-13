4ca1d676bfa1b5fca7851c78b1c1c772ff149b74
Dan Stahlke <dstahlke@gmail.com>
2011-03-13 11:01:24 -0400

    more package metadata

--- setup.py
+++ setup.py
@@ -34,11 +34,44 @@ class test_qitensor(Command):
 setup(
     name = 'qitensor',
     version = qitensor.__version__,
-    description = 'Quantum Tensors',
     author = 'Dan Stahlke',
     author_email = 'dstahlke@gmail.com',
     url = 'http://www.stahlke.org/dan/qitensor',
     license = 'BSD',
+    keywords = ['quantum', 'tensor', 'numpy'],
+    description = 'Quantum Tensors',
+    long_description = '''
+This module is essentially a wrapper for numpy that uses semantics useful for
+finite dimensional quantum mechanics of many particles.  In particular, this
+should be useful for the study of quantum information and quantum computing.
+Each array is associated with a tensor-product Hilbert space.  The underlying
+spaces can be bra spaces or ket spaces and are indexed using any finite
+sequence (typically a range of integers starting from zero, but any sequence is
+allowed).  When arrays are multiplied, a tensor contraction is performed among
+the bra spaces of the left array and the ket spaces of the right array.
+Various linear algebra methods are available which are aware of the Hilbert
+space tensor product structure.
+
+* Component Hilbert spaces have string labels (e.g. ``qubit('a') * qubit('b')``
+  gives ``|a,b>``).
+* Component spaces are finite dimensional and are indexed either by integers or
+  by any sequence (e.g. elements of a group).
+* In Sage, it is possible to create arrays over the Symbolic Ring.
+* Multiplication of arrays automatically contracts over the intersection of the
+  bra space of the left factor and the ket space of the right factor.
+* Linear algebra routines such as SVD are provided which are aware of the
+  Hilbert space labels.
+    ''',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        ],
     packages = [
         'qitensor',
         'qitensor.tests',

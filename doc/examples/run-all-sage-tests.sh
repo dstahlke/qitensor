a590edeac8b4a3c9ca96449e77679c34502c86b5
Dan Stahlke <dan@stahlke.org>
2012-11-14 13:17:10 -0500

    doctest fix

--- doc/examples/run-all-sage-tests.sh
+++ doc/examples/run-all-sage-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./run-sage-test.sh group_indices.rst
-./run-sage-test.sh symbolic.rst
+./run-sage-test.sh symbolic-sage.rst
 ./run-sage-test.sh graph_codes.rst
 ./run-sage-test.sh pickle_sage.rst

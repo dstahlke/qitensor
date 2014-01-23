b0aa7026d92ab1b10fca27a23a32497ed9f7b9c4
Dan Stahlke <dan@stahlke.org>
2014-01-23 14:21:09 -0500

    cvxopt is indeed ported to python3

--- qitensor/experimental/__init__.py
+++ qitensor/experimental/__init__.py
@@ -2,7 +2,4 @@ import six
 
 from . import cartan_decompose
 from . import stabilizers
-
-# FIXME: cvxopt not ported to python3?
-if six.PY2:
-    from . import noncommgraph
+from . import noncommgraph
